644e8bc16e98813c71f450e8466ec1d7c9dfa412
Pascal Weber <weber-pascal@web.de>
2021-06-16 16:14:18 +0200

    add FavoritePage

---
+++ src/pages/FavoritePage.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+import Button from './../components/Button'
+import Header from './../components/Header'
+
+FavoritePage.propTypes = {
+  onNavigate: PropTypes.func.isRequired,
+}
+
+export default function FavoritePage({
+  onNavigate,
+  details: { title, main_image_path, öffnungszeiten, beschreibung },
+}) {
+  return (
+    <>
+      <div>
+        <BackButton onClick={onNavigate}>&lt; back</BackButton>
+      </div>
+      <Header>Details</Header>
+      <Wrapper>
+        <h2>{title}</h2>
+        <img src={main_image_path} alt="" />
+        <h3>Öffnungszeiten:</h3>
+        <ul>
+          {öffnungszeiten.map(öffnungszeit => (
+            <li>{öffnungszeit}</li>
+          ))}
+        </ul>
+        <h3>Beschreibung:</h3>
+        <p>{beschreibung}</p>
+      </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  justify-items: center;
+  max-width: 600px;
+  padding: 10px;
+  color: whitesmoke;
+
+  img {
+    width: 350px;
+    border: 2px solid white;
+    border-radius: 10px;
+  }
+  li {
+    list-style-type: none;
+  }
+`
+const BackButton = styled(Button)`
+  position: absolute;
+  left: 2em;
+  top: 3em;
+`
---
+++ src/pages/FavoritePage.stories.js
@@ -0,0 +1,10 @@
+import FavoritePage from './FavoritePage'
+export default {
+  title: 'FavoritePage',
+  component: FavoritePage,
+}
+
+const Template = args => <FavoritePage {...args} />
+
+export const Default = Template.bind({})
+Default.args = {}
