b47efdfb0140c77f3fd7bb4f80f2f3ab1e9ee26e
Pascal Weber <weber-pascal@web.de>
2021-06-30 18:33:11 +0200

    ...

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -2,21 +2,21 @@ import styled from 'styled-components/macro'
 import PropTypes from 'prop-types'
 import background from './../Images/augustusplatz.jpg'
 import backgroundHeader from './../Images/Logo.png'
-import Slider from '../components/Slider'
-import data from './../image.json'
+// import Slider from '../components/Slider'
+// import data from './../image.json'
 
 StartPage.propTypes = {
   main_image_path: PropTypes.string,
 }
 
 export default function StartPage() {
-  const imageArray = data.map(data => data.main_image_path)
+  // const imageArray = data.map(data => data.main_image_path)
   return (
     <Wrapper>
       <h1>
         <span></span>
       </h1>
-      {/* <Slider images={imageArray} /> */}
+
       <p>
         Willkommen in Leipzig. Hier kannst du coole Locations finden und zu
         deinen Favoriten hinzufügen. Viel Spaß beim Erkunden!
